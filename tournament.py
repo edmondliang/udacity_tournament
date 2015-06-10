60e9ae258a1d5a5e5dcd6317dd53c3efd56e81ea
edmondliang <edmond.liang.sf@gmail.com>
2015-06-10 13:21:18 -0700

    refactor:
    refactored function "connect()" and modified other database operation.

--- tournament.py
+++ tournament.py
@@ -10,32 +10,38 @@ score_type = {"win": 3, "lose": 0, "draw": 1, "bye": 3}
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    database_name = "tournament"
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("<error message>")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("delete from matches")
+    conn, cur = connect()
+    query = "truncate matches RESTART IDENTITY"
+    cur.execute(query)
     conn.commit()
     conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("delete from players")
+    conn, cur = connect()
+    query = "truncate players RESTART IDENTITY"
+    cur.execute(query)
     conn.commit()
     conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("select count(*) as num from players")
+    conn, cur = connect()
+    query = "select count(*) as num from players"
+    cur.execute(query)
     row = cur.fetchone()
     conn.close()
     return row[0]
@@ -50,9 +56,10 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("insert into players(name) values(%s)", (name,))
+    conn, cur = connect()
+    query = "insert into players(name) values(%s)"
+    params = (name,)
+    cur.execute(query, params)
     conn.commit()
     conn.close()
 
@@ -60,8 +67,8 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or
+    a player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -71,9 +78,9 @@ def playerStandings():
         matches: the number of matches the player has played
     """
     """Returns the number of players currently registered."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("select id,name,wins,matches from playerStandings")
+    conn, cur = connect()
+    query = "select id,name,wins,matches from playerStandings"
+    cur.execute(query)
     result = cur.fetchall()
     conn.close()
     return result
@@ -94,12 +101,13 @@ def reportMatch(winner, loser, draw=False):
         is_win = 0
         is_lose = 0
 
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""insert into matches(player_id,opponent_id,is_win,is_lose,is_draw,is_bye,
-        score) values(%s,%s,%s,%s,%s,0,%s)""", (winner, loser, is_win, is_lose, is_draw, score_type['win']),)
-    cur.execute("""insert into matches(player_id,opponent_id,is_win,is_lose,is_draw,is_bye,
-        score) values(%s,%s,%s,%s,%s,0,%s)""", (loser, winner, is_lose, is_win, is_draw, score_type['lose']),)
+    conn, cur = connect()
+    query = """insert into matches(player_id,opponent_id,is_win,is_lose,is_draw,is_bye,
+        score) values(%s,%s,%s,%s,%s,0,%s)"""
+    params1 = (winner, loser, is_win, is_lose, is_draw, score_type['win'],)
+    params2 = (loser, winner, is_lose, is_win, is_draw, score_type['lose'],)
+    cur.execute(query, params1)
+    cur.execute(query, params2)
     conn.commit()
     conn.close()
 
@@ -119,13 +127,12 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT a.id AS id1,
-               a.name AS name1,
-               b.id AS id2,
-               b.name AS name2
+    conn, cur = connect()
+    query = """
+        SELECT a.id AS player1,
+               a.name as name1,
+               b.id AS player2,
+               b.name as name2
         FROM playersranking a, playersranking b
         WHERE (NOT exists
             (SELECT *
@@ -134,9 +141,9 @@ def swissPairings():
                AND b.id=matches.opponent_id) or (b.id=matches.player_id
                AND a.id=matches.opponent_id) ))
           AND a.id<b.id
-          and a.rid<b.rid
-          and a.rank>=b.rank;
-        """)
+          and a.rank>=b.rank
+        """
+    cur.execute(query)
     result = cur.fetchall()
     conn.close()
     return result
